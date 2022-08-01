4807eba8b6d850ffac298c11898bd51e52dd65ea
Gipoong <gipoong@naver.com>
2022-08-01 21:41:37 +0900

    SEQUENCE 전략 적용

--- src/main/java/com/rubypaper/biz/domain/Employee.java
+++ src/main/java/com/rubypaper/biz/domain/Employee.java
@@ -7,10 +7,15 @@ import javax.persistence.*;
 @Data
 @Entity
 @Table(name = "S_EMP")
+@SequenceGenerator(name = "S_EMP_GENERATOR",
+                    sequenceName = "S_EMP_SEQUENCE",
+                    initialValue = 1,
+                    allocationSize = 1)
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+                    generator = "S_EMP_GENERATOR")
     private Long id;
 
     private String name;
